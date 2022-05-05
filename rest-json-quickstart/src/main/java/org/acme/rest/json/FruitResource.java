2a52801ce54f0713d01cbbe1a10763c1b46ba2eb
Binod Suman <binodkumarsuman@gmail.com>
2022-05-05 13:09:51 +0530

    Backend service for Quarkus Reactive demo

---
+++ rest-json-quickstart/src/main/java/org/acme/rest/json/FruitResource.java
@@ -0,0 +1,52 @@
+package org.acme.rest.json;
+
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Set;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+
+import io.smallrye.mutiny.Uni;
+
+@Path("/fruits")
+public class FruitResource {
+
+    private Set<Fruit> fruits = Collections.newSetFromMap(Collections.synchronizedMap(new LinkedHashMap<>()));
+
+    public FruitResource() {
+        fruits.add(new Fruit("Apple", "Winter fruit"));
+        fruits.add(new Fruit("Pineapple", "Tropical fruit"));
+    }
+
+    @GET
+    public Set<Fruit> list() {
+    	try {
+			Thread.sleep(5000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        return fruits;
+    }
+    
+    @Path("reactive")
+    @GET
+    public Uni<Set<Fruit>> listReactive() {
+        return Uni.createFrom().item(fruits);
+    }
+
+    @POST
+    public Set<Fruit> add(Fruit fruit) {
+        fruits.add(fruit);
+        return fruits;
+    }
+
+    @DELETE
+    public Set<Fruit> delete(Fruit fruit) {
+        fruits.removeIf(existingFruit -> existingFruit.name.contentEquals(fruit.name));
+        return fruits;
+    }
+}
